f7993cdbd95b494dcec806630882d43da8fad23c
Byh73 <oneel2be@gmail.com>
2025-03-05 21:01:02 +0600

    Bomber changed

--- src/kg/geeks/game/players/Bomber.java
+++ src/kg/geeks/game/players/Bomber.java
@@ -1,18 +1,18 @@
 package kg.geeks.game.players;
 
 public class Bomber extends Hero {
+    private boolean hasExploded = false;
+
     public Bomber(String name, int health, int damage) {
         super(name, health, damage, SuperAbility.BOMB);
     }
 
     @Override
     public void applySuperPower(Boss boss, Hero[] heroes) {
-        if (this.getHealth() <= 0) {
+        if (this.getHealth() <= 0 && !hasExploded) {
             boss.setHealth(boss.getHealth() - 100);
-            System.out.println("boss get 100 explode damage!");
+            hasExploded = true;
+            System.out.println(this.getName() + " has exploded,100 damage to the boss!");
         }
-
-
     }
 }
-
